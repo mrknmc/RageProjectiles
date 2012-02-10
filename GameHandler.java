f226cbe216b2165171096c367ba8ea2fa9fb8acc
Ryan Davies <ryanrenniedavies@gmail.com>
2012-02-10 19:22:37 +0800

    Now generates target

--- GameHandler.java
+++ GameHandler.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GameHandler {
     
@@ -6,17 +7,19 @@ public class GameHandler {
 	Scanner input = new Scanner( System.in);
 	int x = 1;
 	int y = 0;
-
+	Random generator = new Random();
+	double xDiff = (generator.nextDouble()) * 50 + 100;
+	double yDiff = (generator.nextDouble()) * 50 + 100;
 	while (x <= 3 && y == 0) {
 	    System.out.println ("Guess: " + x);
 	    System.out.print ("Angle: ");
 	    double angle = input.nextDouble();
 	    System.out.print ("Speed: ");
 	    double speed = input.nextDouble();
-	    System.out.print ("X Distance: ");
+	    /*System.out.print ("X Distance: ");
 	    double xDiff = input.nextDouble();
 	    System.out.print ("Y Distance: ");
-	    double yDiff = input.nextDouble();
+	    double yDiff = input.nextDouble();*/
 	     y = AngryBirds.calculate(angle,speed,xDiff,yDiff);
 	    x++;
 	    
