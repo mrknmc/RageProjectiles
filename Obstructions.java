8853ef2a564a94b9fa5554b452602c5b09d1d36a
Ryan Davies <ryanrenniedavies@gmail.com>
2012-02-16 19:37:39 +0000

    Obstructions Object

---
+++ Obstructions.java
@@ -0,0 +1,43 @@
+public class Obstructions {
+
+		// Attributes for the object. I think these are all we decided on,
+		// if there are more just let me know.
+		private int width;
+		private int height;
+		private Point position; // The Point and Color data types will need 
+		private Color colour;   // to be declared seperately.
+		
+
+		// Methods for getting the value of attributes.
+		public int getWidth(){
+			return this.width;
+		}
+		
+		public int getHeight(){
+			return this.height;
+		}
+		
+		public Point getPosition(){
+				return this.position;
+		}
+		
+		public void setPosition(Point newPoint){
+				this.position = newPoint;
+		}
+		
+		public void destroyed(){
+		// Not really sure what should go here.
+				
+		}
+		
+
+		// Constructor
+		public Obstructions(int w, int h, Point p, Color c){
+			this.width = w;
+			this.height = h;
+			this.position = p;
+			this.colour = c;
+			
+		}
+		
+}
