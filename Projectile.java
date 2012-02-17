3b7603d66d0c37b0a0a1e03ae7465970db4e4a31
s1127762 <totalsteph@hotmail.co.uk>
2012-02-17 11:53:46 +0000

    Update Projectile.java

---
+++ Projectile.java
@@ -0,0 +1,40 @@
+
+public class ProjectileBirds {
+  
+
+			// Attributes for the object
+			private int width;
+			private int height;
+			private weight; 
+			private colour; 
+
+
+			// Methods for getting the value of attributes
+			public int getWidth(){
+			return this.width;
+			}
+			
+			public int getHeight(){
+			return this.height; 
+			}
+
+			public int getWeight(){
+			return this.weight;
+			}
+
+			//public void 
+			//this.
+			//}
+			
+			// Not too sure what I should be putting here
+
+
+			// Constructor
+			public ProjectileBirds(int w, int h, color c){
+			this.width = w;
+			this.height = h;
+			this.colour = c;
+
+			}
+
+			}
