fb6242fffd90536e84b5b2df5c366e066150398a
Mark Nemec <mrknmc@me.com>
2012-02-17 15:52:42 +0000

    update projectile

--- Projectile.java
+++ Projectile.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+import java.awt.Point;
 
 public class Projectile {
   
@@ -45,23 +45,4 @@ public class Projectile {
 
 			}
 
-			}
-=======
-import java.awt.Point;
-
-public class Projectile {
-	int size;
-	Point origin;
-	Point leftBottomCorner;
-	Point rightTopCorner;
-	Point rightBottomCorner;
-	
-	public Projectile(Point aPoint, int aSize) {
-		origin = aPoint;
-		size = aSize;
-		leftBottomCorner = new Point(origin.x, origin.y+size);
-		rightTopCorner = new Point(origin.x+size, origin.y);
-		rightBottomCorner = new Point(origin.x+size, origin.y+size);
-	}
 }
->>>>>>> world
